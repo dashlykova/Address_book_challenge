2ba6426d7043be4bf6f6922dbd651a23db5e295d
Daria Lykova <daria.lykova@gmail.com>
2019-06-19 17:48:09 +0200

    Getting 'TypeError: Cannot read property 'close' of undefined' error message at every test run

--- src/app.js
+++ src/app.js
@@ -1,73 +1,68 @@
-const storage = window.localStorage
+const storage = window.localStorage;
+
 const renderContacts = () => {
-    const storage = window.localStorage
     const contacts = JSON.parse(storage.getItem('contacts'))
-  
-    let div = document.querySelector('.contact-list')
-        if (contacts) {
-        div.innerHTML = ''
-  
-      const ul = document.createElement('ul')
-
-      contacts.forEach(contact => {
-        let li = document.createElement('li')
-        li.innerHTML = `
-          <div class="card">
-            <div class="image">
-              <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
-            </div>
-            <div class="content">
-              <h1>${ contact.name }</h1>
-              <h2>${ contact.company }</h2>
-              <p>${ contact.notes }</p> 
-              ${ contact.email } | 
-              <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
-            </div>
-          </div>
-       `
-        ul.appendChild(li)
-      })
-  
-      div.appendChild(ul) 
-    } else { 
-      div.innerHTML = '<p>You have no contacts in your address book</p>' 
-    }
-  }
-
     
-document.addEventListener('DOMContentLoaded', () => {
-    const addContactForm = document.querySelector('.new-contact-form')
-    renderContacts()
+    let div = document.querySelector('.contact-list');
+    div.innderHTML = ''
+    if (contacts) {        
+        const ul = document.createElement('ul')
+        ul.className = "list-reset";
+        
+        contacts.forEach(contact => {
+            let li = document.createElement('li')
+            li.innerHTML = `
+                <div class="card">
+                    <div class="content">
+                        <h1>${ contact.name }</h1>
+                        <h2>${ contact.company }</h2>
+                        <p>${ contact.notes }</p>
+                        ${ contact.email } |
+                        <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+                    </div>
+                </div>
+            `
+            ul.appendChild(li)
+        })
+        
+        div.appendChild(ul)
+    } else {
+        div.innerHTML = `<p>You have no contacts in your address book</p>`
+    }
+}
 
+document.addEventListener('DOMContentLoaded', () => {    
+    renderContacts()
+    const addContactForm = document.querySelector('.new-contact-form')
     addContactForm.addEventListener('submit', event => {
-      event.preventDefault()
-  
-      const {
-        name,
-        email,
-        phone,
-        company,
-        notes,
-        twitter,
-      } = addContactForm.elements
-  
-      const contact = {
-        id: Date.now(),
-        name: name.value,
-        email: email.value,
-        phone: phone.value,
-        company: company.value,
-        notes: notes.value,
-        twitter: twitter.value,
-      }
-  
-      storage.setItem('contacts', JSON.stringify([contact]))
+        let clearMessage = document.querySelector('.contact-list');
+        clearMessage.innerHTML = ""
+        event.preventDefault()        
+        const storage = window.localStorage;    
+        const{
+            name,
+            email,
+            phone,
+            company,
+            notes,
+            twitter,
+        } = addContactForm.elements
+
+        const contact ={
+            id: Date.now(),
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            company: company.value,
+            notes: notes.value,
+            twitter: twitter.value,
+        }
+        console.log(`Saving the following contact: ${JSON.stringify(contact)}`);
+        
         let contacts = JSON.parse(storage.getItem('contacts')) || []
         contacts.push(contact)
         storage.setItem('contacts', JSON.stringify(contacts))
-        renderContacts() 
-        console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
-      
+        renderContacts()
+        addContactForm.reset()
     })
-    })
-
+})
