fdfce88f0adb8a53a031d16ee0596583aa60fca2
Daria Lykova <daria.lykova@gmail.com>
2019-06-19 14:19:13 +0200

    Creating checkContactStorage count helper method

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -9,23 +9,20 @@ Given('I visit the site', async function() {
 });
 
 Then('I should see {string}', async function(content) {
-  // Write code here that turns the phrase above into concrete actions
   return await this.pageHasTextContent(content);
 });
 
 When('I click {string}', async function(string) {
-  // Write code here that turns the phrase above into concrete actions
-  return await this.clickOnButton()
+  return await this.clickOnButton(string)
 });
 
 Then('I fill in {string} with {string}', async function(field, content) {
-  // Write code here that turns the phrase above into concrete actions
   return await this.fillFormField(field.toLowerCase(), content)
 });
 
-Then('I should have {int} contact in my address book', async function(int) {
+Then('I should have {int} contact in my address book', async function(contactCount) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.checkContactStorageCount(contactCount)
 });
 
 Then('I should not see {string}', async function(string) {
